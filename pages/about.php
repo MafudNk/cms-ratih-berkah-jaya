4c0e6598dfbddfbf67eaa5dae3a1c802e15155b6
Fauzi Brillian <brillianfauzi@gmail.com>
2024-01-15 22:40:41 +0700

    feat:aboutUs Page

--- pages/about.php
+++ pages/about.php
@@ -8,7 +8,7 @@
     <div class="preloader d-flex align-items-center justify-content-center">
         <div class="preloader-circle"></div>
         <div class="preloader-img">
-            <img src="../img/core-img/leaf.png" alt="">
+            <img src="../img/core-img/logo.jpg" alt="">
         </div>
     </div>
 
@@ -19,7 +19,7 @@
     <!-- ##### Breadcrumb Area Start ##### -->
     <div class="breadcrumb-area">
         <!-- Top Breadcrumb Area -->
-        <div class="top-breadcrumb-area bg-img bg-overlay d-flex align-items-center justify-content-center" style="background-image: url(../img/bg-img/24.jpg);">
+        <div class="top-breadcrumb-area bg-img bg-overlay d-flex align-items-center justify-content-center" style="background-image: url(../img/bg-img/bg1.jpg);">
             <h2>ABOUT US</h2>
         </div>
 
@@ -166,219 +166,59 @@
             </div>
 
             <div class="row align-items-center justify-content-between">
-                <div class="col-12 col-lg-5">
-                    <div class="alazea-service-area mb-100">
+            <div class="col-12 col-lg-5">
+                <div class="alazea-service-area mb-100">
 
-                        <!-- Single Service Area -->
-                        <div class="single-service-area d-flex align-items-center">
-                            <!-- Icon -->
-                            <div class="service-icon mr-30">
-                                <img src="img/core-img/s1.png" alt="">
-                            </div>
-                            <!-- Content -->
-                            <div class="service-content">
-                                <h5>Plants Care</h5>
-                                <p>In Aenean purus, pretium sito amet sapien denim moste consectet sedoni urna placerat sodales.service its.</p>
-                            </div>
+                    <div class="single-service-area d-flex align-items-center wow fadeInUp" data-wow-delay="100ms">
+                        <!-- Icon -->
+                        <div class="service-icon mr-30">
+                            <img src="http://localhost/cms-ratih-berkah-jaya/img/core-img/world.png" alt="">
                         </div>
-
-                        <!-- Single Service Area -->
-                        <div class="single-service-area d-flex align-items-center">
-                            <!-- Icon -->
-                            <div class="service-icon mr-30">
-                                <img src="img/core-img/s2.png" alt="">
-                            </div>
-                            <!-- Content -->
-                            <div class="service-content">
-                                <h5>Pressure Washing</h5>
-                                <p>In Aenean purus, pretium sito amet sapien denim moste consectet sedoni urna placerat sodales.service its.</p>
-                            </div>
+                        <!-- Content -->
+                        <div class="service-content">
+                            <h5>International Quality Standards</h5>
+                            <p>.</p>
                         </div>
-
-                        <!-- Single Service Area -->
-                        <div class="single-service-area d-flex align-items-center">
-                            <!-- Icon -->
-                            <div class="service-icon mr-30">
-                                <img src="img/core-img/s3.png" alt="">
-                            </div>
-                            <!-- Content -->
-                            <div class="service-content">
-                                <h5>Tree Service &amp; Trimming</h5>
-                                <p>In Aenean purus, pretium sito amet sapien denim moste consectet sedoni urna placerat sodales.service its.</p>
-                            </div>
-                        </div>
-
                     </div>
+
+                    <!-- Single Service Area -->
+                    <div class="single-service-area d-flex align-items-center wow fadeInUp" data-wow-delay="300ms">
+                        <!-- Icon -->
+                        <div class="service-icon mr-30">
+                            <img src="http://localhost/cms-ratih-berkah-jaya/img/core-img/reputation.png" alt="">
+                        </div>
+                        <!-- Content -->
+                        <div class="service-content">
+                            <h5>Customized Export Solutions</h5>
+                            <p>.</p>
+                        </div>
+                    </div>
+
+                    <!-- Single Service Area -->
+                    <div class="single-service-area d-flex align-items-center wow fadeInUp" data-wow-delay="500ms">
+                        <!-- Icon -->
+                        <div class="service-icon mr-30">
+                            <img src="http://localhost/cms-ratih-berkah-jaya/img/core-img/best-price.png" alt="">
+                        </div>
+                        <!-- Content -->
+                        <div class="service-content">
+                            <h5>Competitive Pricing</h5>
+                            <p>.</p>
+                        </div>
+                    </div>
+
                 </div>
+            </div>
 
                 <div class="col-12 col-lg-6">
-                    <div class="alazea-video-area bg-overlay mb-100">
-                        <img src="img/bg-img/23.jpg" alt="">
-                        <a href="http://www.youtube.com/watch?v=7HKoqNJtMTQ" class="video-icon">
-                            <i class="fa fa-play" aria-hidden="true"></i>
-                        </a>
-                    </div>
+                    <a class="nav-brand"><img src="http://localhost/cms-ratih-berkah-jaya/img/core-img/logo-removebg-preview.png" alt=""></a>
                 </div>
             </div>
         </div>
     </section>
     <!-- ##### Service Area End ##### -->
 
-    <!-- ##### Testimonial Area Start ##### -->
-    <section class="testimonial-area section-padding-100">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="testimonials-slides owl-carousel">
 
-                        <!-- Single Testimonial Slide -->
-                        <div class="single-testimonial-slide">
-                            <div class="row align-items-center">
-                                <div class="col-12 col-md-6">
-                                    <div class="testimonial-thumb">
-                                        <img src="img/bg-img/13.jpg" alt="">
-                                    </div>
-                                </div>
-                                <div class="col-12 col-md-6">
-                                    <div class="testimonial-content">
-                                        <!-- Section Heading -->
-                                        <div class="section-heading">
-                                            <h2>TESTIMONIAL</h2>
-                                            <p>Some kind words from clients about Alazea</p>
-                                        </div>
-                                        <p>“Alazea is a pleasure to work with. Their ideas are creative, they came up with imaginative solutions to some tricky issues, their landscaping and planting contacts are equally excellent we have a beautiful but also manageable garden as a result. Thank you!”</p>
-                                        <div class="testimonial-author-info">
-                                            <h6>Mr. Nick Jonas</h6>
-                                            <p>CEO of NAVATECH</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Single Testimonial Slide -->
-                        <div class="single-testimonial-slide">
-                            <div class="row align-items-center">
-                                <div class="col-12 col-md-6">
-                                    <div class="testimonial-thumb">
-                                        <img src="img/bg-img/14.jpg" alt="">
-                                    </div>
-                                </div>
-                                <div class="col-12 col-md-6">
-                                    <div class="testimonial-content">
-                                        <!-- Section Heading -->
-                                        <div class="section-heading">
-                                            <h2>TESTIMONIAL</h2>
-                                            <p>Some kind words from clients about Alazea</p>
-                                        </div>
-                                        <p>“Alazea is a pleasure to work with. Their ideas are creative, they came up with imaginative solutions to some tricky issues, their landscaping and planting contacts are equally excellent we have a beautiful but also manageable garden as a result. Thank you!”</p>
-                                        <div class="testimonial-author-info">
-                                            <h6>Mr. Nazrul Islam</h6>
-                                            <p>CEO of NAVATECH</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Single Testimonial Slide -->
-                        <div class="single-testimonial-slide">
-                            <div class="row align-items-center">
-                                <div class="col-12 col-md-6">
-                                    <div class="testimonial-thumb">
-                                        <img src="img/bg-img/15.jpg" alt="">
-                                    </div>
-                                </div>
-                                <div class="col-12 col-md-6">
-                                    <div class="testimonial-content">
-                                        <!-- Section Heading -->
-                                        <div class="section-heading">
-                                            <h2>TESTIMONIAL</h2>
-                                            <p>Some kind words from clients about Alazea</p>
-                                        </div>
-                                        <p>“Alazea is a pleasure to work with. Their ideas are creative, they came up with imaginative solutions to some tricky issues, their landscaping and planting contacts are equally excellent we have a beautiful but also manageable garden as a result. Thank you!”</p>
-                                        <div class="testimonial-author-info">
-                                            <h6>Mr. Jonas Nick</h6>
-                                            <p>CEO of NAVATECH</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- ##### Testimonial Area End ##### -->
-
-    <!-- ##### Cool Facts Area Start ##### -->
-    <section class="cool-facts-area bg-img section-padding-100-0" style="background-image: url(img/bg-img/cool-facts.png);">
-        <div class="container">
-            <div class="row">
-
-                <!-- Single Cool Facts Area -->
-                <div class="col-12 col-sm-6 col-md-3">
-                    <div class="single-cool-fact d-flex align-items-center justify-content-center mb-100">
-                        <div class="cf-icon">
-                            <img src="img/core-img/cf1.png" alt="">
-                        </div>
-                        <div class="cf-content">
-                            <h2><span class="counter">20</span></h2>
-                            <h6>AWARDS</h6>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Single Cool Facts Area -->
-                <div class="col-12 col-sm-6 col-md-3">
-                    <div class="single-cool-fact d-flex align-items-center justify-content-center mb-100">
-                        <div class="cf-icon">
-                            <img src="img/core-img/cf2.png" alt="">
-                        </div>
-                        <div class="cf-content">
-                            <h2><span class="counter">70</span></h2>
-                            <h6>PROJECTS</h6>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Single Cool Facts Area -->
-                <div class="col-12 col-sm-6 col-md-3">
-                    <div class="single-cool-fact d-flex align-items-center justify-content-center mb-100">
-                        <div class="cf-icon">
-                            <img src="img/core-img/cf3.png" alt="">
-                        </div>
-                        <div class="cf-content">
-                            <h2><span class="counter">30</span>+</h2>
-                            <h6>HAPPY CLIENTS</h6>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Single Cool Facts Area -->
-                <div class="col-12 col-sm-6 col-md-3">
-                    <div class="single-cool-fact d-flex align-items-center justify-content-center mb-100">
-                        <div class="cf-icon">
-                            <img src="img/core-img/cf4.png" alt="">
-                        </div>
-                        <div class="cf-content">
-                            <h2><span class="counter">80</span>K+</h2>
-                            <h6>REVENUE</h6>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-
-        <!-- Side Image -->
-        <div class="side-img wow fadeInUp" data-wow-delay="500ms">
-            <img src="img/core-img/pot.png" alt="">
-        </div>
-    </section>
-    <!-- ##### Cool Facts Area End ##### -->
 
     <!-- ##### Team Area Start ##### -->
     <section class="team-area section-padding-100-0">
@@ -400,14 +240,7 @@
                     <div class="single-team-member text-center mb-100">
                         <!-- Team Member Thumb -->
                         <div class="team-member-thumb">
-                            <img src="img/bg-img/team1.png" alt="">
-                            <!-- Social Info -->
-                            <div class="team-member-social-info">
-                                <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                                <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                                <a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
-                                <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                            </div>
+                            <img src="http://localhost/cms-ratih-berkah-jaya/img/bg-img/team1.png" alt="">
                         </div>
                         <!-- Team Member Info -->
                         <div class="team-member-info mt-30">
@@ -422,14 +255,7 @@
                     <div class="single-team-member text-center mb-100">
                         <!-- Team Member Thumb -->
                         <div class="team-member-thumb">
-                            <img src="img/bg-img/team2.png" alt="">
-                            <!-- Social Info -->
-                            <div class="team-member-social-info">
-                                <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                                <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                                <a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
-                                <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                            </div>
+                            <img src="http://localhost/cms-ratih-berkah-jaya/img/bg-img/team2.png" alt="">
                         </div>
                         <!-- Team Member Info -->
                         <div class="team-member-info mt-30">
@@ -444,14 +270,7 @@
                     <div class="single-team-member text-center mb-100">
                         <!-- Team Member Thumb -->
                         <div class="team-member-thumb">
-                            <img src="img/bg-img/team3.png" alt="">
-                            <!-- Social Info -->
-                            <div class="team-member-social-info">
-                                <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                                <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                                <a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
-                                <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                            </div>
+                            <img src="http://localhost/cms-ratih-berkah-jaya/img/bg-img/team3.png" alt="">
                         </div>
                         <!-- Team Member Info -->
                         <div class="team-member-info mt-30">
@@ -466,14 +285,7 @@
                     <div class="single-team-member text-center mb-100">
                         <!-- Team Member Thumb -->
                         <div class="team-member-thumb">
-                            <img src="img/bg-img/team4.png" alt="">
-                            <!-- Social Info -->
-                            <div class="team-member-social-info">
-                                <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                                <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                                <a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
-                                <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                            </div>
+                            <img src="http://localhost/cms-ratih-berkah-jaya/img/bg-img/team4.png" alt="">
                         </div>
                         <!-- Team Member Info -->
                         <div class="team-member-info mt-30">
